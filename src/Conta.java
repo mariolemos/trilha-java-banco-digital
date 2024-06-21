f84fefa28981e82b5b34e8f27db38059523acd3c
lemos <lemos>
2024-06-21 16:28:06 -0300

    Desenvolvendo métodos saque, depósito, transferência e testando a aplicação.

--- src/Conta.java
+++ src/Conta.java
@@ -7,6 +7,10 @@ public abstract class Conta implements IConta{
     protected int numero;
     protected double saldo;
 
+    public Conta() {
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
 
     @Override
     public void sacar(double valor) {
@@ -22,7 +26,12 @@ public abstract class Conta implements IConta{
     @Override
     public void transferir(double valor, Conta contaDestino) {
         sacar(valor);
-        depositar(valor);
+        contaDestino.depositar(valor);
+    }
+    protected void imprimirDadosComuns() {
+        System.out.println(String.format("Agência: %d", agencia));
+        System.out.println(String.format("Número: %d", numero));
+        System.out.println(String.format("Saldo: %2f", saldo));
     }
 
     public int getAgencia() {
--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,7 +1,8 @@
 public class ContaCorrente extends Conta {
 
-    public ContaCorrente() {
-
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Corrente ===");
+        super.imprimirDadosComuns();
     }
-
 }
